d0459ba26b8c8b523bef3b5e15d3e0656322414b
Jameson Asher <163117394+zaWarudo06@users.noreply.github.com>
2024-03-12 11:21:00 +0200

    Create codemain.cs

---
+++ codemain.cs
@@ -0,0 +1,25 @@
+ protected Model(int width, int height, int N, bool periodic, Heuristic heuristic)
+    {
+        MX = width;
+        MY = height;
+        this.N = N;
+        this.periodic = periodic;
+        this.heuristic = heuristic;
+    }
+
+    void Init()
+    {
+        wave = new bool[MX * MY][];
+        compatible = new int[wave.Length][][];
+        for (int i = 0; i < wave.Length; i++)
+        {
+            wave[i] = new bool[T];
+            compatible[i] = new int[T][];
+            for (int t = 0; t < T; t++) compatible[i][t] = new int[4];
+        }
+        distribution = new double[T];
+        observed = new int[MX * MY];
+
+        weightLogWeights = new double[T];
+        sumOfWeights = 0;
+        sumOfWeightLogWeights = 0;
